9303fa0935e0792bf1a02bc1d7fbc955168e99b8
Michael <mmichaeli2004@gmail.com>
2026-08-25 17:31:51 +0300

    Complete Phase 3 traffic object collection

---
+++ Code/TrafficObjectList.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+namespace TheSharpTurn
+{
+    [Serializable]
+    public class TrafficObjectList
+    {
+        protected SortedList trafficObjects;
+
+        public TrafficObjectList()
+        {
+            trafficObjects = new SortedList();
+        }
+
+        public int NextIndex
+        {
+            get
+            {
+                return trafficObjects.Count;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return trafficObjects.Count;
+            }
+        }
+
+        public TrafficObject this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= trafficObjects.Count)
+                    return (TrafficObject)null;
+
+                return (TrafficObject)trafficObjects.GetByIndex(index);
+            }
+            set
+            {
+                if (index >= 0 && index <= trafficObjects.Count && value != null)
+                    trafficObjects[index] = value;
+            }
+        }
+
+        public void Add(TrafficObject obj)
+        {
+            if (obj != null)
+                this[NextIndex] = obj;
+        }
+
+        public void Remove(int element)
+        {
+            if (element >= 0 && element < trafficObjects.Count)
+            {
+                for (int i = element; i < trafficObjects.Count - 1; i++)
+                    trafficObjects[i] = trafficObjects[i + 1];
+
+                trafficObjects.RemoveAt(trafficObjects.Count - 1);
+            }
+        }
+
+        public void Clear()
+        {
+            trafficObjects.Clear();
+        }
+
+        public void DrawAll(Graphics g)
+        {
+            for (int i = 0; i < trafficObjects.Count; i++)
+                ((TrafficObject)trafficObjects.GetByIndex(i)).Draw(g);
+        }
+
+        public bool IsAreaFree(Rectangle bounds)
+        {
+            return IsAreaFree(bounds, null);
+        }
+
+        public bool IsAreaFree(Rectangle bounds, TrafficObject ignoredObject)
+        {
+            TrafficObject current;
+
+            for (int i = 0; i < trafficObjects.Count; i++)
+            {
+                current = (TrafficObject)trafficObjects.GetByIndex(i);
+
+                if (current != ignoredObject && current.Bounds.IntersectsWith(bounds))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public int CountObjectsInLane(int lane)
+        {
+            int count = 0;
+            TrafficObject current;
+
+            for (int i = 0; i < trafficObjects.Count; i++)
+            {
+                current = (TrafficObject)trafficObjects.GetByIndex(i);
+
+                if (current.Lane == lane)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public TrafficObject FindObjectAt(int xP, int yP)
+        {
+            TrafficObject current;
+
+            for (int i = trafficObjects.Count - 1; i >= 0; i--)
+            {
+                current = (TrafficObject)trafficObjects.GetByIndex(i);
+
+                if (current.Contains(xP, yP))
+                    return current;
+            }
+
+            return (TrafficObject)null;
+        }
+    }
+}
