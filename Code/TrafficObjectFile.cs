4fd54ada2547bcf05e50b50c90573045dc2c6ead
Michael <mmichaeli2004@gmail.com>
2026-08-25 17:49:56 +0300

    Add Phase 4 save and load serialization

---
+++ Code/TrafficObjectFile.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TheSharpTurn
+{
+    public static class TrafficObjectFile
+    {
+        public static void Save(string fileName, TrafficObjectList trafficObjects)
+        {
+            IFormatter formatter = new BinaryFormatter();
+
+            using (Stream stream = new FileStream(fileName, FileMode.Create,
+                FileAccess.Write, FileShare.None))
+            {
+                formatter.Serialize(stream, trafficObjects);
+            }
+        }
+
+        public static TrafficObjectList Load(string fileName)
+        {
+            IFormatter formatter = new BinaryFormatter();
+
+            using (Stream stream = new FileStream(fileName, FileMode.Open,
+                FileAccess.Read, FileShare.Read))
+            {
+                return (TrafficObjectList)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
