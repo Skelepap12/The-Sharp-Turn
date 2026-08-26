6553bd2dbe4432b59ff12c6c4294eef1bd91b9bf
Michael <mmichaeli2004@gmail.com>
2026-08-26 18:24:47 +0300

    Track regular road overtaking return lane

--- Code/TheSharpTurn/Motor Vehicle/RoadUser.cs
+++ Code/TheSharpTurn/Motor Vehicle/RoadUser.cs
@@ -5,15 +5,48 @@ namespace TheSharpTurn
     [Serializable]
     public abstract class RoadUser : TrafficObject
     {
+        int overtakeReturnLane;
+        bool isOvertaking;
+
         public RoadUser()
             : base()
-        { }
+        {
+            OvertakeReturnLane = -1;
+            IsOvertaking = false;
+        }
 
         public RoadUser(int xVal, int yVal, int laneVal,
             TravelDirection directionVal, int desiredSpeedVal,
             int actualSpeedVal, int widthVal, int heightVal)
             : base(xVal, yVal, laneVal, directionVal, desiredSpeedVal,
                   actualSpeedVal, widthVal, heightVal)
-        { }
+        {
+            OvertakeReturnLane = -1;
+            IsOvertaking = false;
+        }
+
+        public int OvertakeReturnLane
+        {
+            get
+            {
+                return overtakeReturnLane;
+            }
+            set
+            {
+                overtakeReturnLane = value;
+            }
+        }
+
+        public bool IsOvertaking
+        {
+            get
+            {
+                return isOvertaking;
+            }
+            set
+            {
+                isOvertaking = value;
+            }
+        }
     }
 }
