2771c62fd900d72b6a18f449a6a3073c83e6a8b4
Michael <mmichaeli2004@gmail.com>
2026-08-26 18:56:16 +0300

    Move Form1 designer to GUI folder

---
+++ Code/TheSharpTurn/GUI/Form1.Designer.cs
@@ -0,0 +1,300 @@
+namespace TheSharpTurn
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.buttonManual = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelSelected = new System.Windows.Forms.Label();
+            this.labelSelectedTitle = new System.Windows.Forms.Label();
+            this.numSpeed = new System.Windows.Forms.NumericUpDown();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.comboModel = new System.Windows.Forms.ComboBox();
+            this.labelModel = new System.Windows.Forms.Label();
+            this.comboType = new System.Windows.Forms.ComboBox();
+            this.labelType = new System.Windows.Forms.Label();
+            this.labelObjectMenu = new System.Windows.Forms.Label();
+            this.pictureBoxMap = new System.Windows.Forms.PictureBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.panelControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(12, 10);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(206, 32);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "THE SHARP TURN";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(1084, 14);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(80, 30);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(1170, 14);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(80, 30);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // panelControls
+            // 
+            this.panelControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelControls.Controls.Add(this.buttonManual);
+            this.panelControls.Controls.Add(this.buttonDelete);
+            this.panelControls.Controls.Add(this.buttonAdd);
+            this.panelControls.Controls.Add(this.labelSelected);
+            this.panelControls.Controls.Add(this.labelSelectedTitle);
+            this.panelControls.Controls.Add(this.numSpeed);
+            this.panelControls.Controls.Add(this.labelSpeed);
+            this.panelControls.Controls.Add(this.comboModel);
+            this.panelControls.Controls.Add(this.labelModel);
+            this.panelControls.Controls.Add(this.comboType);
+            this.panelControls.Controls.Add(this.labelType);
+            this.panelControls.Controls.Add(this.labelObjectMenu);
+            this.panelControls.Location = new System.Drawing.Point(12, 58);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(250, 580);
+            this.panelControls.TabIndex = 3;
+            // 
+            // buttonManual
+            // 
+            this.buttonManual.Enabled = false;
+            this.buttonManual.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonManual.Location = new System.Drawing.Point(18, 472);
+            this.buttonManual.Name = "buttonManual";
+            this.buttonManual.Size = new System.Drawing.Size(210, 56);
+            this.buttonManual.TabIndex = 11;
+            this.buttonManual.Text = "Manually Control Selected Object";
+            this.buttonManual.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(18, 408);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(210, 48);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "Delete Selected Object";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(18, 212);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(210, 48);
+            this.buttonAdd.TabIndex = 9;
+            this.buttonAdd.Text = "Add Object";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelSelected
+            // 
+            this.labelSelected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelSelected.Location = new System.Drawing.Point(18, 305);
+            this.labelSelected.Name = "labelSelected";
+            this.labelSelected.Size = new System.Drawing.Size(210, 78);
+            this.labelSelected.TabIndex = 8;
+            this.labelSelected.Text = "Selected: none";
+            this.labelSelected.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelSelectedTitle
+            // 
+            this.labelSelectedTitle.AutoSize = true;
+            this.labelSelectedTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelectedTitle.Location = new System.Drawing.Point(14, 278);
+            this.labelSelectedTitle.Name = "labelSelectedTitle";
+            this.labelSelectedTitle.Size = new System.Drawing.Size(127, 19);
+            this.labelSelectedTitle.TabIndex = 7;
+            this.labelSelectedTitle.Text = "SELECTED OBJECT";
+            // 
+            // numSpeed
+            // 
+            this.numSpeed.Location = new System.Drawing.Point(18, 171);
+            this.numSpeed.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSpeed.Name = "numSpeed";
+            this.numSpeed.Size = new System.Drawing.Size(210, 23);
+            this.numSpeed.TabIndex = 6;
+            this.numSpeed.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(15, 150);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(85, 15);
+            this.labelSpeed.TabIndex = 5;
+            this.labelSpeed.Text = "Desired speed";
+            // 
+            // comboModel
+            // 
+            this.comboModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboModel.FormattingEnabled = true;
+            this.comboModel.Location = new System.Drawing.Point(18, 117);
+            this.comboModel.Name = "comboModel";
+            this.comboModel.Size = new System.Drawing.Size(210, 23);
+            this.comboModel.TabIndex = 4;
+            // 
+            // labelModel
+            // 
+            this.labelModel.AutoSize = true;
+            this.labelModel.Location = new System.Drawing.Point(15, 96);
+            this.labelModel.Name = "labelModel";
+            this.labelModel.Size = new System.Drawing.Size(41, 15);
+            this.labelModel.TabIndex = 3;
+            this.labelModel.Text = "Model";
+            // 
+            // comboType
+            // 
+            this.comboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboType.FormattingEnabled = true;
+            this.comboType.Items.AddRange(new object[] {
+            "Car",
+            "Motorcycle",
+            "Bus",
+            "Emergency Vehicle",
+            "Bicycle",
+            "Pedestrian"});
+            this.comboType.Location = new System.Drawing.Point(18, 63);
+            this.comboType.Name = "comboType";
+            this.comboType.Size = new System.Drawing.Size(210, 23);
+            this.comboType.TabIndex = 2;
+            this.comboType.SelectedIndexChanged += new System.EventHandler(this.comboType_SelectedIndexChanged);
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Location = new System.Drawing.Point(15, 42);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(31, 15);
+            this.labelType.TabIndex = 1;
+            this.labelType.Text = "Type";
+            // 
+            // labelObjectMenu
+            // 
+            this.labelObjectMenu.AutoSize = true;
+            this.labelObjectMenu.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelObjectMenu.Location = new System.Drawing.Point(14, 12);
+            this.labelObjectMenu.Name = "labelObjectMenu";
+            this.labelObjectMenu.Size = new System.Drawing.Size(108, 20);
+            this.labelObjectMenu.TabIndex = 0;
+            this.labelObjectMenu.Text = "OBJECT MENU";
+            // 
+            // pictureBoxMap
+            // 
+            this.pictureBoxMap.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.pictureBoxMap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMap.Location = new System.Drawing.Point(280, 58);
+            this.pictureBoxMap.Name = "pictureBoxMap";
+            this.pictureBoxMap.Size = new System.Drawing.Size(970, 580);
+            this.pictureBoxMap.TabIndex = 4;
+            this.pictureBoxMap.TabStop = false;
+            this.pictureBoxMap.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxMap_Paint);
+            this.pictureBoxMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMap_MouseDown);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelStatus.Location = new System.Drawing.Point(12, 651);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(1238, 28);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Ready.";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 691);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.pictureBoxMap);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Sharp Turn";
+            this.panelControls.ResumeLayout(false);
+            this.panelControls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.Label labelObjectMenu;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.ComboBox comboType;
+        private System.Windows.Forms.Label labelModel;
+        private System.Windows.Forms.ComboBox comboModel;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.NumericUpDown numSpeed;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelSelectedTitle;
+        private System.Windows.Forms.Label labelSelected;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonManual;
+        private System.Windows.Forms.PictureBox pictureBoxMap;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
