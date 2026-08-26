b3878b9f5fc7cd1d31057f4bde34e19dc46330a0
Michael <mmichaeli2004@gmail.com>
2026-08-26 22:33:37 +0300

    Make Phase 8 map scale cleanly in fullscreen

--- Code/TheSharpTurn/GUI/Form1.Phase8.cs
+++ Code/TheSharpTurn/GUI/Form1.Phase8.cs
@@ -6,7 +6,11 @@ namespace TheSharpTurn
 {
     public partial class Form1
     {
+        const int LogicalMapWidth = 970;
+        const int LogicalMapHeight = 580;
+
         Button buttonFullscreen;
+        PictureBox pictureBoxDisplay;
         Panel panelFocusMode;
         Label labelFocusModeTitle;
         Label labelFocusModeInstructions;
@@ -20,7 +24,7 @@ namespace TheSharpTurn
             base.OnLoad(e);
 
             windowedBounds = this.Bounds;
-            this.MinimumSize = this.Size;
+            this.MinimumSize = new Size(800, 650);
 
             CreatePhase8Controls();
 
@@ -31,7 +35,6 @@ namespace TheSharpTurn
             buttonAdd.Click += new EventHandler(UpdatePhase8AfterAction);
             buttonDelete.Click += new EventHandler(UpdatePhase8AfterAction);
             buttonLoad.Click += new EventHandler(UpdatePhase8AfterAction);
-            pictureBoxMap.MouseDown += new MouseEventHandler(pictureBoxMap_MouseDown_Phase8);
             simulationTimer.Tick += new EventHandler(simulationTimer_Tick_Phase8);
 
             UpdatePhase8State();
@@ -39,6 +42,18 @@ namespace TheSharpTurn
 
         private void CreatePhase8Controls()
         {
+            pictureBoxDisplay = new PictureBox();
+            pictureBoxDisplay.Name = "pictureBoxDisplay";
+            pictureBoxDisplay.BackColor = pictureBoxMap.BackColor;
+            pictureBoxDisplay.BorderStyle = pictureBoxMap.BorderStyle;
+            pictureBoxDisplay.TabStop = false;
+            pictureBoxDisplay.Paint += new PaintEventHandler(pictureBoxDisplay_Paint);
+            pictureBoxDisplay.MouseDown += new MouseEventHandler(pictureBoxDisplay_MouseDown);
+            this.Controls.Add(pictureBoxDisplay);
+
+            pictureBoxMap.Size = new Size(LogicalMapWidth, LogicalMapHeight);
+            pictureBoxMap.Visible = false;
+
             buttonFullscreen = new Button();
             buttonFullscreen.Name = "buttonFullscreen";
             buttonFullscreen.Size = new Size(105, 30);
@@ -97,11 +112,13 @@ namespace TheSharpTurn
             }
 
             UpdatePhase8Layout();
+            pictureBoxDisplay.Invalidate();
         }
 
         private void Form1_Resize_Phase8(object sender, EventArgs e)
         {
             UpdatePhase8Layout();
+            pictureBoxDisplay.Invalidate();
         }
 
         private void Form1_KeyDown_Phase8(object sender, KeyEventArgs e)
@@ -119,16 +136,110 @@ namespace TheSharpTurn
         private void UpdatePhase8AfterAction(object sender, EventArgs e)
         {
             UpdatePhase8State();
-        }
-
-        private void pictureBoxMap_MouseDown_Phase8(object sender, MouseEventArgs e)
-        {
-            UpdatePhase8State();
+            pictureBoxDisplay.Invalidate();
         }
 
         private void simulationTimer_Tick_Phase8(object sender, EventArgs e)
         {
             UpdatePhase8State();
+            pictureBoxDisplay.Invalidate();
+        }
+
+        private void pictureBoxDisplay_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.FromArgb(35, 35, 35));
+
+            float scale = GetMapScale();
+            if (scale <= 0)
+                return;
+
+            int drawWidth = (int)(LogicalMapWidth * scale);
+            int drawHeight = (int)(LogicalMapHeight * scale);
+            int offsetX = (pictureBoxDisplay.ClientSize.Width - drawWidth) / 2;
+            int offsetY = (pictureBoxDisplay.ClientSize.Height - drawHeight) / 2;
+
+            e.Graphics.TranslateTransform(offsetX, offsetY);
+            e.Graphics.ScaleTransform(scale, scale);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            DrawMap(e.Graphics);
+            trafficObjects.DrawAll(e.Graphics);
+
+            if (curIndex >= 0 && curIndex < trafficObjects.Count)
+            {
+                Rectangle selectedBounds = trafficObjects[curIndex].Bounds;
+                selectedBounds.Inflate(3, 3);
+                e.Graphics.DrawRectangle(Pens.Gold, selectedBounds);
+            }
+        }
+
+        private void pictureBoxDisplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (manualObject != null)
+            {
+                MouseEventArgs exitEvent = new MouseEventArgs(e.Button, e.Clicks,
+                    0, 0, e.Delta);
+                pictureBoxMap_MouseDown(pictureBoxMap, exitEvent);
+                UpdatePhase8State();
+                pictureBoxDisplay.Invalidate();
+                return;
+            }
+
+            int logicalX;
+            int logicalY;
+
+            if (!TryGetLogicalMapPoint(e.X, e.Y, out logicalX, out logicalY))
+                return;
+
+            MouseEventArgs logicalEvent = new MouseEventArgs(e.Button, e.Clicks,
+                logicalX, logicalY, e.Delta);
+            pictureBoxMap_MouseDown(pictureBoxMap, logicalEvent);
+            UpdatePhase8State();
+            pictureBoxDisplay.Invalidate();
+        }
+
+        private float GetMapScale()
+        {
+            if (pictureBoxDisplay.ClientSize.Width <= 0 ||
+                pictureBoxDisplay.ClientSize.Height <= 0)
+                return 0;
+
+            float scaleX = pictureBoxDisplay.ClientSize.Width /
+                (float)LogicalMapWidth;
+            float scaleY = pictureBoxDisplay.ClientSize.Height /
+                (float)LogicalMapHeight;
+
+            return Math.Min(scaleX, scaleY);
+        }
+
+        private bool TryGetLogicalMapPoint(int displayX, int displayY,
+            out int logicalX, out int logicalY)
+        {
+            logicalX = 0;
+            logicalY = 0;
+
+            float scale = GetMapScale();
+            if (scale <= 0)
+                return false;
+
+            int drawWidth = (int)(LogicalMapWidth * scale);
+            int drawHeight = (int)(LogicalMapHeight * scale);
+            int offsetX = (pictureBoxDisplay.ClientSize.Width - drawWidth) / 2;
+            int offsetY = (pictureBoxDisplay.ClientSize.Height - drawHeight) / 2;
+
+            if (displayX < offsetX || displayX >= offsetX + drawWidth ||
+                displayY < offsetY || displayY >= offsetY + drawHeight)
+                return false;
+
+            logicalX = (int)((displayX - offsetX) / scale);
+            logicalY = (int)((displayY - offsetY) / scale);
+
+            if (logicalX >= LogicalMapWidth)
+                logicalX = LogicalMapWidth - 1;
+            if (logicalY >= LogicalMapHeight)
+                logicalY = LogicalMapHeight - 1;
+
+            return true;
         }
 
         private void UpdatePhase8State()
@@ -217,7 +328,7 @@ namespace TheSharpTurn
 
         private void UpdatePhase8Layout()
         {
-            if (pictureBoxMap == null)
+            if (pictureBoxDisplay == null)
                 return;
 
             if (focusModeActive)
@@ -228,51 +339,56 @@ namespace TheSharpTurn
 
         private void UpdateNormalModeLayout()
         {
-            const int ContentWidth = 1238;
-            const int GroupHeight = 669;
+            const int Margin = 12;
             const int SidePanelWidth = 250;
             const int Gap = 18;
+            const int ContentTop = 58;
 
-            int left = (this.ClientSize.Width - ContentWidth) / 2;
-            int top = (this.ClientSize.Height - GroupHeight) / 2;
+            labelTitle.Location = new Point(Margin, 10);
 
-            if (left < 12)
-                left = 12;
-            if (top < 10)
-                top = 10;
+            buttonLoad.Location = new Point(
+                this.ClientSize.Width - Margin - buttonLoad.Width, 14);
+            buttonSave.Location = new Point(
+                buttonLoad.Left - buttonSave.Width - 6, 14);
+            buttonFullscreen.Location = new Point(
+                buttonSave.Left - buttonFullscreen.Width - 6, 14);
 
-            labelTitle.Location = new Point(left, top);
+            int statusTop = this.ClientSize.Height - 40;
+            int contentHeight = statusTop - ContentTop - 13;
 
-            buttonLoad.Location = new Point(left + ContentWidth - buttonLoad.Width,
-                top + 4);
-            buttonSave.Location = new Point(buttonLoad.Left - buttonSave.Width - 6,
-                top + 4);
-            buttonFullscreen.Location = new Point(buttonSave.Left - buttonFullscreen.Width - 6,
-                top + 4);
+            if (contentHeight < 200)
+                contentHeight = 200;
 
-            panelControls.Location = new Point(left, top + 48);
-            pictureBoxMap.Location = new Point(left + SidePanelWidth + Gap, top + 48);
-            pictureBoxMap.Size = new Size(970, 580);
+            panelControls.Location = new Point(Margin, ContentTop);
+            panelControls.Size = new Size(SidePanelWidth, contentHeight);
 
-            labelStatus.Location = new Point(left, top + 641);
-            labelStatus.Size = new Size(ContentWidth, 28);
+            int displayLeft = Margin + SidePanelWidth + Gap;
+            int displayWidth = this.ClientSize.Width - displayLeft - Margin;
+
+            if (displayWidth < 100)
+                displayWidth = 100;
+
+            pictureBoxDisplay.Location = new Point(displayLeft, ContentTop);
+            pictureBoxDisplay.Size = new Size(displayWidth, contentHeight);
+
+            labelStatus.Location = new Point(Margin, statusTop);
+            labelStatus.Size = new Size(this.ClientSize.Width - Margin * 2, 28);
+
+            pictureBoxDisplay.SendToBack();
+            panelControls.BringToFront();
         }
 
         private void UpdateFocusModeLayout()
         {
-            int mapLeft = (this.ClientSize.Width - pictureBoxMap.Width) / 2;
-            int mapTop = (this.ClientSize.Height - pictureBoxMap.Height) / 2;
+            const int Margin = 12;
 
-            if (mapLeft < 0)
-                mapLeft = 0;
-            if (mapTop < 0)
-                mapTop = 0;
+            pictureBoxDisplay.Location = new Point(Margin, Margin);
+            pictureBoxDisplay.Size = new Size(
+                this.ClientSize.Width - Margin * 2,
+                this.ClientSize.Height - Margin * 2);
 
-            pictureBoxMap.Location = new Point(mapLeft, mapTop);
-            pictureBoxMap.Size = new Size(970, 580);
-
-            panelFocusMode.Location = new Point(pictureBoxMap.Left + 12,
-                pictureBoxMap.Top + 12);
+            panelFocusMode.Location = new Point(pictureBoxDisplay.Left + 12,
+                pictureBoxDisplay.Top + 12);
             panelFocusMode.BringToFront();
         }
     }
